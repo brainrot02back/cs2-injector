a427dae1718826b66a788e22e6b10fdb05fff7fd
brainz <228283365+brainrot02back@users.noreply.github.com>
2025-10-17 07:27:09 -0400

    Update injector.h

--- Manual Map Injector/injector.h
+++ Manual Map Injector/injector.h
@@ -7,20 +7,20 @@
 #include <stdio.h>
 #include <string>
 
-using f_LoadLibraryA = HINSTANCE(WINAPI*)(const char* lpLibFilename);
-using f_GetProcAddress = FARPROC(WINAPI*)(HMODULE hModule, LPCSTR lpProcName);
-using f_DLL_ENTRY_POINT = BOOL(WINAPI*)(void* hDll, DWORD dwReason, void* pReserved);
+typedef HINSTANCE(WINAPI* f_LL)(const char* lpLibFilename);
+typedef FARPROC(WINAPI* f_GPA)(HMODULE hModule, LPCSTR lpProcName);
+typedef BOOL(WINAPI* f_DEP)(void* hDll, DWORD dwReason, void* pReserved);
 
 #ifdef _WIN64
-using f_RtlAddFunctionTable = BOOL(WINAPIV*)(PRUNTIME_FUNCTION FunctionTable, DWORD EntryCount, DWORD64 BaseAddress);
+typedef BOOL(WINAPIV* f_RAFT)(PRUNTIME_FUNCTION FunctionTable, DWORD EntryCount, DWORD64 BaseAddress);
 #endif
 
-struct MANUAL_MAPPING_DATA
+struct MM_DATA
 {
-	f_LoadLibraryA pLoadLibraryA;
-	f_GetProcAddress pGetProcAddress;
+	f_LL pLL;
+	f_GPA pGPA;
 #ifdef _WIN64
-	f_RtlAddFunctionTable pRtlAddFunctionTable;
+	f_RAFT pRAFT;
 #endif
 	BYTE* pbase;
 	HINSTANCE hMod;
@@ -29,7 +29,6 @@ struct MANUAL_MAPPING_DATA
 	BOOL SEHSupport;
 };
 
+bool MM_DLL(HANDLE hProc, BYTE* pSrcData, SIZE_T FileSize, bool ClearHeader = true, bool ClearNonNeededSections = true, bool AdjustProtections = true, bool SEHExceptionSupport = true, DWORD fdwReason = DLL_PROCESS_ATTACH, LPVOID lpReserved = 0);
 
-//Note: Exception support only x64 with build params /EHa or /EHc
-bool ManualMapDll(HANDLE hProc, BYTE* pSrcData, SIZE_T FileSize, bool ClearHeader = true, bool ClearNonNeededSections = true, bool AdjustProtections = true, bool SEHExceptionSupport = true, DWORD fdwReason = DLL_PROCESS_ATTACH, LPVOID lpReserved = 0);
-void __stdcall Shellcode(MANUAL_MAPPING_DATA* pData);
+void __stdcall SC(MM_DATA* pData);
